cc530c84b6e2f0e8aa32cfa0aa95fcbdd6f7e57b
etudiant <etudiant@deb12-vdi-rt>
2023-12-12 10:01:21 +0100

    amelioration du code , lecture et debut de creation fichier

--- tests/temp.py
+++ tests/temp.py
@@ -1,7 +1,7 @@
 import openpyxl
 
 # Charger le fichier Excel
-fichier_excel = "/docs/excel_notes/notes_S1/Traiter_des_donnees.xlsx"
+fichier_excel = "/home/etudiant/SAE_105_BILLON_BOUGUEREAU/docs/excel_notes/notes_S1/Traiter_des_donnees.xlsx"
 classeur = openpyxl.load_workbook(fichier_excel)
 
 # Sélectionner la feuille de calcul
@@ -12,6 +12,51 @@ feuille = classeur.active
 for ligne in feuille.iter_rows(min_row=1, max_row=feuille.max_row, values_only=True):
     # `ligne` = valeurs de chaque cellule dans la ligne
     print(ligne)
+    
+    
+
+"""
+wb_out = openpyxl.Workbook()
+#insère une feuille à la fin
+ws1 = wb.create_sheet(title="Feuille1")
+
+
+#ferme le fichier excel
+workbook.close()
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 classeur.close()  # Fermer le classeur
+
+
+
+
